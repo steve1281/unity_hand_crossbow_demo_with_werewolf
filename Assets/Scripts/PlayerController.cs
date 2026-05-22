ee14900037e66abe6e7a739e1e99c7e741cbeb39
Steve Falcigno <steve1281@hotmail.com>
2026-05-22 08:37:31 -0400

    playerscript, basic movement controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Items")]
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private CharacterController charCon;
+    [SerializeField] private InputActionReference jumpAction;
+    [SerializeField] private InputActionReference lookAction;
+
+    [Header("Camera Data")]
+    [SerializeField] private Camera theCam;
+
+    [Header("Locomotion Tuning")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float minLookAngle;
+    [SerializeField] private float maxLookAngle;
+    [SerializeField] private float lookSpeed;
+
+    private float ySpeed;
+    private float horiRot;
+    private float vertRot;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 lookInput = lookAction.action.ReadValue<Vector2>();
+
+        // left/right 
+        horiRot += lookInput.x * Time.deltaTime * lookSpeed;
+        transform.rotation = Quaternion.Euler(0f, horiRot, 0f);
+
+        // up/down
+        vertRot -= lookInput.y * Time.deltaTime * lookSpeed;
+        vertRot = Mathf.Clamp(vertRot, minLookAngle, maxLookAngle);
+        theCam.transform.localRotation = Quaternion.Euler(vertRot, 0f, 0f);
+
+
+        Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
+
+        Vector3 vertMove = transform.forward * moveInput.y;
+        Vector3 horiMove = transform.right * moveInput.x;
+
+        Vector3 moveAmount = horiMove + vertMove;
+        moveAmount = moveAmount.normalized;
+        moveAmount = moveAmount * moveSpeed;
+
+        if (charCon.isGrounded)
+        {
+            ySpeed = 0f;
+
+            if (jumpAction.action.WasPressedThisFrame())
+            {
+                ySpeed = jumpForce;
+            }
+        }
+
+        ySpeed = ySpeed + (Physics.gravity.y * Time.deltaTime);
+
+        moveAmount.y = ySpeed;
+        charCon.Move(moveAmount * Time.deltaTime);
+
+
+    }
+}
